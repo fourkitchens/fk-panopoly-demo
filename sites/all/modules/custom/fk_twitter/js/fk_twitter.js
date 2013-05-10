498012dfddbb935c4aad2a77967d92d3fc7be6f5
Elliott Foster <elliottf@codebrews.com>
2013-05-10 16:36:44 -0500

    Adding 4K twitter module.

---
+++ sites/all/modules/custom/fk_twitter/js/fk_twitter.js
@@ -0,0 +1,58 @@
+/**
+ * @file
+ * JavaScript behaviors for the FPL Twitter module.
+ */
+
+(function($) {
+  Drupal.behaviors.fkTwitter = {
+    attach: function(context, settings) {
+      var rotateDelay = 10000;
+      $('ul.tweets-pulled-listing', context).once('tweets', function() {
+        var tweets = $('li', this);
+        var x = 0;
+        var last = 0;
+
+        /**
+         * Creates a show/hide circular loop on the tweet
+         * list items.
+         */
+        var loopTweets = function() {
+          if (last === x) {
+            if ((x + 1) === tweets.length) {
+              x = 0;
+            }
+            else {
+              x++;
+            }
+            return;
+          }
+
+          $(tweets[last]).fadeOut(function() {
+            $(this).removeClass('shown').addClass('hidden');
+            $(tweets[x]).fadeIn(function() {
+              $(this).removeClass('hidden').addClass('shown');
+              last = x;
+              if ((x + 1) === tweets.length) {
+                x = 0;
+              }
+              else {
+                x++;
+              }
+            });
+          });
+        };
+
+        var loop = setInterval(loopTweets, rotateDelay);
+
+        // Pause and resume the interval on mouse in/out.
+        $(this).mouseover(function() {
+          clearInterval(loop);
+        });
+        $(this).mouseout(function() {
+          loop = setInterval(loopTweets, rotateDelay);
+        });
+      });
+    }
+  };
+}(jQuery));
+
